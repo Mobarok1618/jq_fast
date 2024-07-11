53e7e510adc43225986004e9aa162fc69bb1afcc
Mobarok1618 <159544966+Mobarok1618@users.noreply.github.com>
2024-07-11 21:52:39 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// js start
+
+
+
+
+
+// jq start
+$(document).ready(function(){
+    $('button').click(function(){
+        alert("Welcome!")
+    })
+});
+// // jq start
+// $(document).addClass('text_show').ready(function(){
+//     $('button').click(function(){
+//         alert("Welcome to my web!")
+//     })
+// });
+
+
